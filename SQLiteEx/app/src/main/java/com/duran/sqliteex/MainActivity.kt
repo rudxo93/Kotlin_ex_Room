57c746c688a2123c20183af62e04ac701e5fbfae
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-09 13:48:40 +0900

    SQLite 를 활용한 간단한 예제 만들어보기

--- SQLiteEx/app/src/main/java/com/duran/sqliteex/MainActivity.kt
+++ SQLiteEx/app/src/main/java/com/duran/sqliteex/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.duran.sqliteex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+// SQLite
+// Room
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var db: SQLiteHelperSample
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = SQLiteHelperSample(this)
+
+        val inputArea = findViewById<EditText>(R.id.inputArea)
+
+        val insertBtn = findViewById<Button>(R.id.insert)
+        val getAllBtn = findViewById<Button>(R.id.getAll)
+        val deleteBtn = findViewById<Button>(R.id.deleteAll)
+        val resultBtn = findViewById<TextView>(R.id.resultArea)
+
+        insertBtn.setOnClickListener {
+            val inputText = inputArea.text.toString()
+            db.insert(inputText)
+            inputArea.setText("")
+        }
+
+        getAllBtn.setOnClickListener {
+            val arr = db.getAllData()
+            resultBtn.text = arr.toString()
+        }
+
+        deleteBtn.setOnClickListener {
+            db.deleteAll()
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        db.close()
+    }
+
 }
